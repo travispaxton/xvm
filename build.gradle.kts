764859e938031e3e64d617b9de5798f6fcea60a8
Marcus Lagergren <1062473+lagergren@users.noreply.github.com>
2023-03-23 10:57:58 +0100

    Typos and comments.

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,25 +34,30 @@ tasks.register("build") {
     dependsOn(project("xdk:").tasks["build"])
 }
 
-// gitClean:
-//   This is a task to clean up all files in the source tree that are not under source control,
-//   with the exception of individual IDE configurations.
-//
-//   This task is a dry run; it will only list what it would like to delete and it's recommended
-//    to run this first as a saftety measure. To actually perform the deletions, use the task
-//    "gitCleanLive" instead.
+/*
+ * gitClean:
+ *
+ * This is a task to clean up all files in the source tree that are not under source control,
+ * with the exception of individual IDE configurations.
+ *
+ * This task is a dry run; it will only list what it would like to delete and it's recommended
+ * to run this first as a safety measure. To actually perform the deletions, use the task
+ */
 tasks.register<Exec>("gitClean") {
     group = "other"
     description = "Does a dry run of git clean, recursively from the repo root, but only prints what would be removed. The .idea configuration directory is exempt."
     commandLine("git", "clean", "-nfxd", "-e", ".idea")
 }
 
-// gitClean:
-//   This is a task to clean up all files in the source tree that are not under source control,
-//   with the exception of individual IDE configurations.
-//
-//   Use with caution, and to be safe, it is recommended that you run the task 'gitClean' first,
-//   to get a list of the files and directories that will be purged.
+/*
+ * gitCleanLive:
+ *
+ * This is a task to clean up all files in the source tree that are not under source control,
+ * with the exception of individual IDE configurations.
+ *
+ * WARNING: Use with caution, and to be safe, it is recommended that you run the task 'gitClean'
+ * first, to get a list of the files and directories that will be purged.
+ */
 tasks.register<Exec>("gitCleanLive") {
     group = "other"
     description = "Deletes all files not under source control, recursively from the repo root. The .idea configuration directory is exempt."
